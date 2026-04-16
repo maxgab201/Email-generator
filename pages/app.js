b7a863d6238a2525c4fc02e5316a02476a00ff38
maxgab201 <maxgab201@gmail.com>
2026-04-16 13:10:35 -0300

    Create MyApp component in app.js
    Added a main application component that wraps page components.

---
+++ pages/app.js
@@ -0,0 +1,7 @@
+import '../styles/globals.css' // Verificá que la carpeta styles esté en la raíz
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
