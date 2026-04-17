e864f89098fbb64e69f50011a3733fef00183a39
maxgab201 <maxgab201@gmail.com>
2026-04-17 12:39:47 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css' // Verificá que la carpeta styles esté en la raíz
+import '../styles/globals.css'
+import Head from 'next/head'
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    // Activa el Service Worker
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js');
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#131313" />
+        <link rel="apple-touch-icon" href="https://i.postimg.cc/1XfP6k97/37730-removebg-preview.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
